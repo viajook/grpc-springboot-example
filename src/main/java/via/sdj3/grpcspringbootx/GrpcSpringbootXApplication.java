8cde56f6d8550b5b84768e96accfff07a036bfcf
J Okika <joseph.okika@gmail.com>
2022-10-07 21:58:50 +0200

    gRPC Spring Boot project template (session 7) - initial commit

---
+++ src/main/java/via/sdj3/grpcspringbootx/GrpcSpringbootXApplication.java
@@ -0,0 +1,13 @@
+package via.sdj3.grpcspringbootx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringbootXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcSpringbootXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/via/sdj3/grpcspringbootx/GrpcSpringbootXApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sdj3.grpcspringbootx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringbootXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
